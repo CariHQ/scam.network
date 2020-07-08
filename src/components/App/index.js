4063d319603ce446d2b46cfcff238dbc8800a81f
Kenroy George <kenroy@garakami.com>
2020-07-08 21:24:06 +0100

    first commit

---
+++ src/components/App/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Navigation from '../Navigation';
+import LandingPage from '../Landing';
+import SignUpPage from '../SignUp';
+import SignInPage from '../SignIn';
+import PasswordForgetPage from '../PasswordForget';
+import HomePage from '../Home';
+import AccountPage from '../Account';
+import AdminPage from '../Admin';
+
+import * as ROUTES from '../../constants/routes';
+import { withAuthentication } from '../Session';
+
+import ErrorBoundary from "../../ErrorBoundary"
+
+import { Container } from 'semantic-ui-react';
+
+const App = () => (
+  <ErrorBoundary>
+    <Router>
+      <div>
+        <Navigation />
+        <Container>
+          <Route exact path={ROUTES.LANDING} component={LandingPage} />
+          <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+          <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+          <Route
+            path={ROUTES.PASSWORD_FORGET}
+            component={PasswordForgetPage}
+          />
+          <Route path={ROUTES.HOME} component={HomePage} />
+          <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+          <Route path={ROUTES.ADMIN} component={AdminPage} />
+        </Container>
+      </div>
+    </Router>
+  </ErrorBoundary>
+);
+
+export default withAuthentication(App);
